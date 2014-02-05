9b0bedbcf0e0a27c10cf9892b1744928a6392c69
Mike Lautman <mikeblautman@gmail.com>
2014-02-05 01:24:58 -0500

    linearTimeSVD skeleton

--- HGDP/linearTimeSVD.py
+++ HGDP/linearTimeSVD.py
@@ -1,5 +1,6 @@
-import numpy as numpy
 
+import numpy as numpy
+import random as rand
 
 def rows(A):
 	return len(A)
@@ -9,14 +10,46 @@ def cols(A):
 def checkValidLinearTimeSVD(A , P , c , k):
 	Arows=rows(A)
 	Acols=cols(A)
-	value = (1<=k and k<=c and c<=Acols)
-	value = ()
+	valid = (1<=k and k<=c and c<=Acols)	
+	valid = valid and (sum(P)==1)
+	for i , p in enumerate(P) :
+		valid = valid and p>=0	
+	return valid
+
+new=True
+def selectWitProb(P) : 
+	if new : 
+		rand.seed(1)
+		new = False
+	num = rand.random()
+
+	index=-1
+	if num==0:
+		return 0
+	else :
+		index=-1
+		while ( num > 0 ) :
+			index+=1
+			num  -= P[index]
+		return index
 
 
+# Need to go through and double check column vs rows
 def linearTimeSVD( A , P , c , k ):
-	valid = checkValidLinearTimeSVD(A , P , c , k)
+	C=np.empty([c,cols(A)]) 
+	if (not checkValidLinearTimeSVD(A , P , c , k)):
+		return None
+	for i in range(c):
+		i_t = selectWitProb(P)
+		beta = math.sqrt( c * P[i_t] )
+		for j,val in enumerate(A[i_t,:]):
+			C[i,j]=val/beta
+	C_T=C.transpose()
+	w, V = np.linalg.eig(C_T.dotC)
+	
+	H_k = n.empty([k,])
+	for i in range(k) : 
 
 
-U, s, V = np.linalg.svd(C)
 
 
